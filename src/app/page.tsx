8941ed217333c4808347db2e30d750508ed73a78
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-22 20:29:55 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <h1>Welcome to elib-client-app</h1>
+    <Banner />
   );
 }
