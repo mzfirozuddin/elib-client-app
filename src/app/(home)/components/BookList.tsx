1865837c240478e5b37e38cc63b0929a312f4f93
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-23 21:20:38 +0530

    add suspense

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,10 +2,20 @@ import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+  // Data fetching //: here we use component streaming
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the books!");
+  }
+
+  const books = await response.json();
+  // console.log(books);
   return (
     <div className="max-w-6xl mx-auto grid grid-cols-1 gap-8 md:grid-cols-3">
-      {books.map((book) => (
+      {books.map((book: Book) => (
         <BookCard key={book._id} book={book} />
       ))}
     </div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,17 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 //: In next js 14 page.tsx and layout.tsx are by default server component
 export default async function Home() {
-  // Data fetching
-  const response = await fetch(`${process.env.BACKEND_URL}/books`)
-  if (!response.ok) {
-    throw new Error("An error occurred while fetching the books!")
-  }
-
-  const books = await response.json()
-  // console.log(books);
-
   return (
     <>
       <Banner />
-      <BookList books={books} />
+      {/* here we use component streaming */}
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
