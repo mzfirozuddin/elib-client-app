af5edbe2a3ae8099c3e645f772ca884869d14f45
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-23 15:19:42 +0530

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t mt-10 p-3 text-center">
+      <span>© Bookish Bliss All rights reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
